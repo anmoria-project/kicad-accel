bd7418175e28a2d3201efc515369974c72427d6c
Johann Sollacher <anmoria.project@gmail.com>
2026-08-30 00:06:26 +0200

    fix: Fix jtag_serialwire trstn naming

--- bus_defines.py
+++ bus_defines.py
@@ -141,7 +141,7 @@ bus_jtag_serialwire = [
     DriverType('jtag_serialwire_master[x].tck_swclk', bus_master, 'output', ['jtag_serialwire_slave[x].tck_swclk'], 16, False),
     DriverType('jtag_serialwire_master[x].mosi',      bus_master, 'output', ['jtag_serialwire_slave[x].tdi'], 1, False),
     DriverType('jtag_serialwire_master[x].miso',      bus_master, 'input',  ['jtag_serialwire_slave[x].tdo'], 1, False),
-    DriverType('jtag_serialwire_master[x].trst_n',    bus_master, 'output', ['jtag_serialwire_slave[x].trst_n'], 1, False),
+    DriverType('jtag_serialwire_master[x].trstn',     bus_master, 'output', ['jtag_serialwire_slave[x].trstn'], 1, False),
     DriverType('jtag_serialwire_master[x].sysrst_n',  bus_master, 'output', ['jtag_serialwire_slave[x].sysrst_n'], 1, False),
 
     DriverType('jtag_serialwire_slave[x].tms_swdio',    bus_slave, 'input',  ['jtag_serialwire_master[x].tms_swdio'],  1, False),
